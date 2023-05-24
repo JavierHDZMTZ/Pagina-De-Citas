606baadfd542d7906d0c036e7f0b3fe706352537
JavierHDZMTZ <kingstondarnel@gmail.com>
2023-05-23 20:48:49 -0600

    v0.1
    archivos actuales

---
+++ Inicio/Inicio.js
@@ -0,0 +1,16 @@
+// Redireccionado a pagina inicial al dar click en el logo
+document.getElementById("logo").addEventListener("click", function() {
+    window.location.href = "Inicio.html";
+  });
+//Redireccion a la pagina de inicio de sesion
+document.getElementById("InicioSesion").addEventListener("click", function() {
+    window.location.href = "../Inicio de Sesion/InicioSesion.php";
+});
+//Redireccion a la pagina de registro
+document.getElementById("Registrarse").addEventListener("click", function() {
+    window.location.href = "../Registro/Registro.php";
+});
+//Redireccion a la pagina de informacion sobre nosotros
+document.getElementById("MasInformacion").addEventListener("click", function() {
+    window.location.href ="../Mas Informacion/Masinformacion.php";
+});
