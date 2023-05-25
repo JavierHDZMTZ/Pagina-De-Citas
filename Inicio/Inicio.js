f7dedf44a6cacf1a11aff394efba147d9370164a
JavierHDZMTZ <kingstondarnel@gmail.com>
2023-05-24 20:24:33 -0600

    Inicio y feed 100% con estilos (No perfecto pero funcional)
    estilos completados

--- Inicio/Inicio.js
+++ Inicio/Inicio.js
@@ -4,7 +4,7 @@ document.getElementById("logo").addEventListener("click", function() {
   });
 //Redireccion a la pagina de inicio de sesion
 document.getElementById("InicioSesion").addEventListener("click", function() {
-    window.location.href = "../Inicio de Sesion/InicioSesion.php";
+    window.location.href = "../Inicio de Sesion/InicioSesion.html";
 });
 //Redireccion a la pagina de registro
 document.getElementById("Registrarse").addEventListener("click", function() {
